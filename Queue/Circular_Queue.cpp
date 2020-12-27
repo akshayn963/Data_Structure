c2d49e37a8e2884a866a1f74e9181aca366abbe0
akshayn963 <65156650+akshayn963@users.noreply.github.com>
2020-12-27 14:15:00 +0530

    Add files via upload

--- Queue/Circular_Queue.cpp
+++ Queue/Circular_Queue.cpp
@@ -1,154 +1,136 @@
 #include<iostream>
-#include<stdlib.h>
-#define SIZE 5
+
+
 using namespace std;
+void insert();
+void delet();
+int rear=-1;// insert element to rear side 
+int front =-1;// delete
+#define size 3
+int queue[size];
 
 
-class CircularQueue
+void  insert()
 {
-private:
-    int arr[5];
-    int front;
-    int rear;
-    int count;
+	int input;
+	cout<<"enetr elemnet"<<endl;
+	cin>>input;
+	if(rear==-1 && front==-1)
+	{
+		rear=front=0;
+		queue[rear]=input;
+	cout<<"insert="<<queue[rear]<<endl;
+	}
+	
+	else if(((rear+1)%size)==front)
+	{
+		cout<<"queue is full";
+	}
+	
+	else
+	{
+		rear=(rear+1)%size;
+		
+	queue[rear]=input;
+	cout<<"insert"<<queue[rear]<<endl;
+	}
+}
 
-public:
+void delet()
+{
+	if(rear==-1 && front==-1)
+	{
+		cout<<"is empty"<<endl<<"reinsert element="<<endl;
+	}
+	
+	else if(front==rear)// at 3rd time both equal
+	{
 
-    CircularQueue()
-    {
-        count=0;
-        front= -1;
-        rear = -1;
-        cout<<"Calling const --------->"<<endl;
-        for(int i=1;i<5;i++)
-        {
-            arr[i]=0;
-        }
-    }
+		cout<<"delete"<<queue[front]<<endl; // delete 3rd element here 
+		front=rear=-1;
 
-    int isfull()
-    {
-        if((rear+1)%5 == front)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
+	}
+	
+	else
+	{
+		
+		
+		front=(front)%size;
+		cout<<"delete="<<queue[front]<<endl;
+		front++;
+	}
+}
+void display()
+{
+	if (front == -1) 
+    { 
+        cout<<"\nQueue is Empty"<<endl; 
+        return; 
+    } 
+    cout<<"\nElements in Circular Queue are: "; 
+    if (rear >= front) 
+    { 
+        for (int i = front; i <= rear; i++) 
+            cout<<queue[i]<<endl; 
+    } 
+    else
+    { 
+        for (int i = front; i < size; i++) 
+            cout<<queue[i]; 
+  
+        for (int i = 0; i <= rear; i++) 
+            cout<<queue[i]; 
+    } 
+	
+	}
+	    
 
-    }
-
-    int isempty()
-    {
-        if(front== -1 && rear== -1)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-
-    }
-
-
-    void enqueue(int input)
-    {
-        if(isfull())
-        {
-            cout<<"Queue is full !!!"<<endl;
-            return;
-        }
-        else if(isempty())
-        {
-            front=0;
-            rear=0;
-            arr[rear]=input;
-            count++;
-        }
-        else
-        {
-            rear=(rear+1)%5;
-            arr[rear]=input;
-            count++;
-        }
-    }
-
-
-    int dequeue()
-    {
-        int a,x=0;
-        a=isempty();
-        if(a)
-        {
-            cout<<"Queue is empty"<<endl;
-            return x;
-        }
-        else if(rear == front)
-        {
-            x=arr[rear];
-            front = -1;
-            rear =-1;
-            count--;
-            return x;
-
-        }
-        else
-        {
-            cout<<"Poped element is : "<<front<<endl;
-            x=arr[front];
-            arr[front]=0;
-            front=(front +1)%5;
-            count--;
-            return x;
-        }
-
-    }
-
-    void display()
-    {
-        int i;
-            cout<<"Queue elements are : ";
-            for( i=0;i<5;i++)
-        {
-            cout<<arr[i]<<" ";
-        }
-    }
-};
 
 int main()
 {
-    CircularQueue q1;
-    int r,input;
-    do
-    {
+	
+	int ch;
+	do
+	{
+	
+	cout<<"Enter ur choice :"<<endl;
+	cout<<"1.Enqueue"<<endl;
+	cout<<"2.Dequeue"<<endl;
+	cout<<"3.Display"<<endl;
+	cout<<"4.Exit"<<endl;
 
-      cout<<"1.push"<<endl<<"2.pop"<<endl<<"3.display"<<endl<<"4.Exit"<<endl<<"Enter your choice : "<<endl;
-      
-      cin>>r;
-      switch(r)
-      {
-      case 1 :
-      cout<<"Enter no to be push : ";
-      cin>>input;
-      q1.enqueue(input);
-      break;
+	cin>>ch;
 
+	switch(ch)
+	{
+		
+		case 1:
+				insert();
+                  break;
+                  
+        case 2:
+        	  	delet();
+        	  	break;
+        case 3:
+         	
+        	  	display();
+        	  	break;
+        
+         case 4:
+         	
+        	  	exit(1);
+        	  	break;
+        	  	
+        default:
+        	cout<<"Invalide choice";
 
-      case 2 : q1.dequeue();
-      break;
+                  	
+	}
+	
 
-      case 3 : q1.display();
-      break;
-      
-      case 4: exit(1);
-
-      default:
-      cout<<"invalid choice !!!!!"<<endl;
-      break;
-      }
-    }while(r!=0);
-return 0;
-
+} while(ch!=0);
+	
+	return 0;
+	
+	
 }
--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,97 +1,99 @@
- #include<iostream>
+#include<iostream>
 
 using namespace std;
-#define SIZE 5
 
-int arr[SIZE];
-int front = -1;
-int rear  = -1;
+#define size 5
 
-int isempty()
-{
-    if(front == -1 && rear == -1)
-    {
-        return 1;
+void enqueue();
+void dequeue();
+void peek();
+void display();
+
+int queue[size];
+int front=-1;
+int rear=-1;
+int x;
+
+int main(){
+     while(true){
+        int ch;
+        cout<<"press 1.enqueue 2.dequeue 3.display 4.peek "<<endl;
+        cin>>ch;
+        switch (ch)
+        {
+        case 1:
+            enqueue();
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            peek();
+            break;
+        default:
+            cout<<"invalid input "<<endl;
+            break;
+        }
+
     }
-    else
-    {
-        return 0;
+    
+}
+
+void enqueue(){
+    cout<<"enter the data = ";
+    cin>>x;
+    if(rear==size-1){
+        cout<<"queue is full...u cant enter data "<<endl;
+    }
+    else if(front==-1 && rear==-1){
+        front=rear=0;
+        queue[rear]=x;
+    }
+    else{
+        rear++;
+        queue[rear]=x;
     }
 }
 
-void enqueue(int input)
-{
-        if(rear == SIZE-1)
-        {
-            cout<<"Queue is full...";
-        }
-        else
-        if(front == -1)
-        {
-            front =0;
-        }
-            rear++;
-
-            arr[rear]=input;
-            
-
-}
-
-void dequeue()
-{
-    if(isempty())
-    {
-        cout<<"Queue is empty";
+void dequeue(){
+    
+    if(front==-1 && rear==-1){
+        cout<<"queue is empty . no data in queue "<<endl;
     }
-    else if(front == rear)
-    {
-        front = rear = -1;
+    else if(front==rear){
+        front=rear=-1;
     }
-    else
-    {
+    else{
         front++;
     }
+        
+    
 }
 
-void displayQueue()
-{
-    if(isempty())
-    {
-        cout<<"queue is Empty ";
+void display(){
+    
+    if(front==-1 && rear==-1){
+        cout<<"queue is empty "<<endl;
     }
-    else
-    for (int i=front;i<=rear;i++)
-    {
-        cout<<arr[i]<<" ";
+    else{
+        for(int i=front;i<rear+1;i++){
+            cout<<queue[i]<<" ";
+        }
+        cout<<endl;
     }
+  
+   
 }
 
-int main()
-{
- int choice, flag=1, input;
- while( flag == 1)
- {
-        cout<<endl<<"Select An Operation : ";
-    	cout<<endl<<"1. Enqueue  "<<endl;
-    	cout<<"2. Dequeue"<<endl;
-    	cout<<"3. Display"<<endl;
-    	cout<<"4. Exit"<<endl;
-  cin>>choice;
-  switch (choice)
-  {
-  case 1: cout<<"Enter Value:\n";
-          cin>>input;
-          enqueue(input);
-          break;
-  case 2: dequeue();
-          break;
-  case 3: displayQueue();
-          break;
-  case 4: flag = 0;
-          break;
-
-  }
- }
-
- return 0;
+void peek(){
+    if(front==-1 && rear==-1){
+        cout<<"queue is empty . no data in queue "<<endl;
+    }
+    else{
+        cout<<queue[rear]<<endl;
+    }
+   
 }
